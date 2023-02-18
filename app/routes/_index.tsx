0f2fdc5d629f1b05e7fde9fdbd4721a7360d4f2e
Scott Bessler <scott.bessler@ironcladhq.com>
2023-02-17 16:18:55 -0800

    redir

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,16 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import dayjs from "dayjs";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const yesterday = dayjs().add(-1, "day").format("YYYY-MM-DD");
+  if (url.pathname === "/") {
+    return redirect(`/scoreboard/${yesterday}`);
+  }
+  return null;
+};
 
 export default function IndexRoute() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"),require("daisyui")],
   // daisyUI config (optional)
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
+  // daisyui: {
+  //   styled: true,
+  //   themes: true,
+  //   base: true,
+  //   utils: true,
+  //   logs: true,
+  //   rtl: false,
+  //   prefix: "",
+  //   darkTheme: "dark",
+  // },
 };
