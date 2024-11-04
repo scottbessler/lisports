06a62b678ef45ccc989ee8dd19af39904c81acef
Scott Bessler <scottbessler@gmail.com>
2024-11-03 21:29:11 -0800

    proxy

--- app/reqs.ts
+++ app/reqs.ts
@@ -1,21 +1,50 @@
+import Proxifly from 'proxifly';
+
 export type ReqResult =
 	| { success: false; response: Response; data?: unknown; jsonError?: unknown }
 	| { success: true; response: Response; data: unknown };
+
+async function initProxy() {
+	const options = {
+		protocol: 'http', // http | socks4 | socks5
+		anonymity: 'anonymous', // transparent | anonymous | elite
+		country: 'US', // https://www.nationsonline.org/oneworld/country_code_list.htm
+		https: true, // true | false
+		speed: 10000, // 0 - 60000
+		quantity: 1, // 1 - 20
+	};
+
+	const proxifly = new Proxifly();
+
+	console.log({ result: await proxifly.getProxy() });
+}
 
 // const jsonMime = "application/json";
 export async function getJSON(
 	url: string,
 	options?: Omit<RequestInit, 'method'>,
 ): Promise<ReqResult> {
+	const proxyInfo = {
+		proxy: 'http://185.238.228.243:80',
+		protocol: 'http',
+		ip: '185.238.228.243',
+		port: 80,
+		https: false,
+		anonymity: 'elite',
+		score: 1,
+		geolocation: { country: 'ES', city: '' },
+	};
 	const headers = new Headers({ ...options?.headers });
 
-	const opts: RequestInit = {
+	const opts = {
 		...options,
 		headers: { ...options?.headers },
 		method: 'GET',
+		proxy: 'http://185.238.228.243:80',
 	};
 	const start = Date.now();
 	console.log('starting', url, headers, opts);
+
 	const response = await fetch(url, opts);
 	console.log('done', url, headers, opts, Date.now() - start);
 	let jsonResult: unknown;
